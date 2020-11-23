438f7d642cb3742a5b9a4ec96c31c59bebcdfaf0
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-11-22 23:18:43 -0500

    added shared memory based gpu histogram kernal

--- flemingHistogram.cu
+++ flemingHistogram.cu
@@ -26,4 +26,29 @@ __global__ void histogram(int* input, int* histogram, int size) {
 	}
 }	
 
+//currently does not work for block sizes smaller than 256
+__global__ void sharedHistogram(int* input, int* histogram, int size) {
+	//initialize shared memory for the block
+	__shared__ int privateHistogram[256];
 
+	if(threadIdx.x < 256) privateHistogram[threadIdx.x] = 0;
+	__syncthreads();
+
+	//get starting index for thread
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	//calculate stride
+	int stride = blockDim.x * gridDim.x;
+	//preform histogram calculation
+	while( i < size) {
+		atomicAdd( &(privateHistogram[input[i]], 1));
+		i += stride;
+	}
+
+	//ensure all threads have finished their additions
+	__syncthreads();
+
+	//add private histogram to public histogram
+	if(threadIdx.x < 256) {
+		atomixAdd( &(histogram[threadIdx.x]), privateHistogram[threadIdx.x]);
+	}
+}
