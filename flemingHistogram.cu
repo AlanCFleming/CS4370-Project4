4ee03c873bb8f096e85e9f984e9d03e56e97e9bc
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-11-23 11:30:14 -0500

    Added code to test kernals

--- flemingHistogram.cu
+++ flemingHistogram.cu
@@ -86,19 +86,109 @@ int main() {
 	float cpuTime= (float(t2-t1)/CLOCKS_PER_SEC*1000);
 
 	//Allocate memory on GPU compution. dev_b is used to store the results of the first pass of reduction
-	int *dev_input, *dev_basicGPU, *dev_sharedGPU;
+	int *dev_input, *dev_basicGPUResult, *dev_sharedGPUResult;
 	cudaMalloc((void **)(&dev_input), MATRIXSIZE *sizeof(int));
-	cudaMalloc((void **)(&dev_basicGPU), 256 *sizeof(int));
-	cudaMalloc((void **)(&dev_sharedGPU), 256 *sizeof(int));
+	cudaMalloc((void **)(&dev_basicGPUResult), 256 *sizeof(int));
+	cudaMalloc((void **)(&dev_sharedGPUResult), 256 *sizeof(int));
 
 	//copy memory to gpu
 	cudaMemcpy(dev_input, input, MATRIXSIZE * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_basicGPU, basicGPUResult, 256 * sizeof(int), cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_sharedGPU, sharedGPUResult, 256 * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_basicGPUResult, basicGPUResult, 256 * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_sharedGPUResult, sharedGPUResult, 256 * sizeof(int), cudaMemcpyHostToDevice);
 
 	//calculate dimentions for gpu
 	dim3 dimBlock(BLOCKSIZE);
 	dim3 dimGrid(ceil(double(MATRIXSIZE)/dimBlock.x));
+
+	//~~WITHOUT SHARED MEMORY~~
+	//Set up cuda events for recording runtime
+	cudaEvent_t start,stop;
+	float basicGPUTime; 
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start,0);
+					
+	//calculate histogram without shared memory
+	histogram<<<dimGrid, dimBlock>>>(dev_input, dev_basicGPUResult, MATRIXSIZE);
+						
+	//calculate runtime 
+	cudaEventRecord(stop,0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&basicGPUTime,start,stop);
+
+	//destroy cuda events
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	//copy sum from gpu
+	cudaMemcpy(basicGPUResult, dev_basicGPUResult, 256 * sizeof(int), cudaMemcpyDeviceToHost);
+
+	//print speedup
+	printf("--WITHOUT SHARED MEMORY--\nCPU Runtime: %f\nGpu Runtime: %f\nSpeedup: %f\n", (double)cpuTime, (double)basicGPUTime, double(cpuTime / basicGPUTime));
+
+	//verify results
+	bool valid = true;
+	for(int i = 0; i < MATRIXSIZE; i++) {	
+		if(cpuResult[i] != basicGPUResult[i]) {
+			valid = false;
+			break;
+		}
+	}
+	if(valid) {
+		printf("TEST PASSED\n");
+	} else {
+		printf("TEST FAILED\n");
+	}
+
+	//~~WITH SHARED MEMORY~~
+	//Set up cuda events for recording runtime
+	cudaEvent_t start,stop;
+	float sharedGPUTime; 
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start,0);
+					
+	//calculate histogram with shared memory
+	histogram<<<dimGrid, dimBlock>>>(dev_input, dev_sharedGPUResult, MATRIXSIZE);
+						
+	//calculate runtime 
+	cudaEventRecord(stop,0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&sharedGPUTime,start,stop);
+
+	//destroy cuda events
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
+
+	//copy sum from gpu
+	cudaMemcpy(sharedGPUResult, dev_sharedGPUResult, 256 * sizeof(int), cudaMemcpyDeviceToHost);
+
+	//print speedup
+	printf("--WITH SHARED MEMORY--\nCPU Runtime: %f\nGpu Runtime: %f\nSpeedup: %f\n", (double)cpuTime, (double)sharedGPUTime, double(cpuTime / sharedGPUTime));
+
+	//verify results
+	bool valid = true;
+	for(int i = 0; i < MATRIXSIZE; i++) {	
+		if(cpuResult[i] != sharedGPUResult[i]) {
+			valid = false;
+			break;
+		}
+	}
+	if(valid) {
+		printf("TEST PASSED\n");
+	} else {
+		printf("TEST FAILED\n");
+	}
+	
+
+	//free up memory before returning
+	free(input);
+	free(cpuResult);
+	free(basicGPUResult);
+	free(sharedGPUResult);
+	cudaFree(dev_input);
+	cudaFree(dev_basicGPUResult);
+	cudaFree(dev_sharedGPUResult);
 
 	return 0;
 }
